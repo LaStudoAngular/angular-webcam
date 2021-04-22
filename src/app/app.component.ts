569035f377db8c53abfd0f54995aa6ef15e1ca18
Moe Greene <gearmobile@gmail.com>
2021-04-22 22:26:25 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
 
 @Component({
   selector: 'cam-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-webcam';
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
+
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
+  }
 }
